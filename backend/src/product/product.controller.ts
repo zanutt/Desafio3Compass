123416ab1a70c1d63e300e33805496e9070c0d25
zanutt <matszg@live.com>
2025-06-30 12:52:36 -0300

    feat: add findAll products route

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+  @Get('/all')
+  async FindAllProducts(@Query('count') count: string) {
+    return this.productService.findAll(parseInt(count) || 8);
+  }
+}
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(@InjectRepository(Product) private repo: Repository<Product>) {}
+  findAll(count) {
+    return this.repo.find({
+      take: count,
+    });
+  }
+}
