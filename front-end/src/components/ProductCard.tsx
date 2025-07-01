acb578f69b4606807609cff73cee00374c61c759
zanutt <matszg@live.com>
2025-07-01 15:32:04 -0300

    feat: finish home page

--- front-end/src/components/ProductCard.tsx
+++ front-end/src/components/ProductCard.tsx
@@ -6,21 +6,38 @@ interface Props {
   product: Product;
 }
 
-const ProductCard: React.FC<Props> = ({ product }) => (
-  <div className={styles.card}>
-    <img src={product.image} alt={product.name} className={styles.image} />
-    <div className={styles.body}>
-      <span className={styles.name}>{product.name}</span>
-      <span className={styles.alias}>{product.alias}</span>
-      <span className={styles.price}>
-        R$ {product.price}
-        {product.descount > 0 && (
-          <span className={styles.discount}> -{product.descount}%</span>
+const ProductCard: React.FC<Props> = ({ product }) => {
+  // func para calcular o desocnto se houver
+  const hasDiscount = product.descount > 0;
+  const discountedPrice = hasDiscount
+    ? (product.price * (1 - product.descount / 100)).toFixed(2)
+    : product.price.toFixed(2);
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.badgeWrapper}>
+        {hasDiscount && (
+          <span className={styles.badgeDiscount}>-{product.descount}%</span>
         )}
-      </span>
-      {product.new && <span className={styles.badgeNew}>Novo!</span>}
+        {!hasDiscount && product.new && (
+          <span className={styles.badgeNew}>New!</span>
+        )}
+      </div>
+      <img src={product.image} alt={product.name} className={styles.image} />
+      <div className={styles.body}>
+        <span className={styles.name}>{product.name}</span>
+        <span className={styles.alias}>{product.alias}</span>
+        <span className={styles.priceRow}>
+          <span className={styles.price}>R$ {discountedPrice}</span>
+          {hasDiscount && (
+            <span className={styles.priceOriginal}>
+              R$ {product.price.toFixed(2)}
+            </span>
+          )}
+        </span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ProductCard;
