cd5566e835a0753da3a914a3d1e891607e34bcc7
zanutt <matszg@live.com>
2025-06-30 22:02:47 -0300

    feat: add initial routes structure

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import SingleProduct from "./pages/SingleProduct";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
